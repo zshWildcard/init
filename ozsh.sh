46c26e91bbc8e5bc6eab1e9f56de69a3da330719
zhangshuohua <zshprint@gmail.com>
2023-09-17 15:31:17 +0800

    fixed:change ozsh install option

--- ozsh.sh
+++ ozsh.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/zsh
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
@@ -8,3 +7,4 @@ sed -i 's/plugins=(git)/plugins=(z git zsh-syntax-highlighting zsh-autosuggestio
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' /root/.zshrc
 
 source ~/.zshrc
+chsh -s /bin/zsh
