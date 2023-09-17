02818e5bfb4b7e590d9c6a8eb5cb69dc43d4e24e
zhangshuohua <zshprint@gmail.com>
2023-09-17 11:59:00 +0800

    feat:change order of

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ apt update
 apt install curl vim ssh git zsh apt-transport-https ca-certificates gnupg-agent software-properties-common -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+./ozsh.sh
 
 # change hostname from concole
 echo "Enter the new hostname"
@@ -17,11 +18,17 @@ echo "$new_hostname" > /etc/hostname
 # set new hostname
 hostname "$new_hostname"
 
-chmod u+x ./*.sh
+chmod u+x ./ssh.sh
+chmod u+x ./ozsh.sh
+chmod u+x ./docker.sh
+chmod u+x ./end.sh
+
 
 # you can adjust the order
 ./ssh.sh
-./ozsh.sh
 ./docker.sh
-./end.sh
+#./end.sh
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+./ozsh.sh
+reboot
--- ozsh.sh
+++ ozsh.sh
@@ -5,6 +5,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sed -i 's/plugins=(git)/plugins=(z git zsh-syntax-highlighting zsh-autosuggestions)/g' /root/.zshrc
-sed -i 's/ZSH_THEME="random"/ZSH_THEME="ys"/g' /root/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' /root/.zshrc
 
 source ~/.zshrc
