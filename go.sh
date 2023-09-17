2980b5b7c13eae4513ac1f9a5347e379f898f612
zhangshuohua <zshprint@gmail.com>
2023-09-17 21:31:43 +0800

    feat:add go and jdk shell

--- go.sh
+++ go.sh
@@ -3,6 +3,6 @@
 
 
 sudo wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-sudo export PATH=$PATH:/usr/local/go/bin
-sudo source ~/.profile
+export PATH=$PATH:/usr/local/go/bin
+source ~/.profile
 go version
--- jdk.sh
+++ jdk.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 sudo apt install openjdk17-jdk
 java --version
