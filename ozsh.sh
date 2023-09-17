85b50ec1152b841d41dc118b9e189d717f84aa8a
zhangshuohua <zshprint@gmail.com>
2023-09-17 15:39:34 +0800

    fixed:use root permission to find option

--- ozsh.sh
+++ ozsh.sh
@@ -3,8 +3,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-sed -i 's/plugins=(git)/plugins=(z git zsh-syntax-highlighting zsh-autosuggestions)/g' /root/.zshrc
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' /root/.zshrc
+sudo sed -i 's/plugins=(git)/plugins=(z git zsh-syntax-highlighting zsh-autosuggestions)/g' /root/.zshrc
+sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' /root/.zshrc
 
 source ~/.zshrc
 chsh -s /bin/zsh
